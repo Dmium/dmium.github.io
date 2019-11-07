116da5b5c9f6a3b843255fdc65e6c9d78833d2e0
Dom Hall <dominiczy.hall@gmail.com>
2019-11-07 15:49:14 +0000

    Basic homepage

---
+++ domhall/src/components/Project.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-left">
+    <b-jumbotron bg-variant="dark" text-variant="light">
+        <b-row>
+            <b-col/>
+            <b-col cols=8>
+            <h1>{{name}}</h1>
+            </b-col>
+            <b-col/>
+        </b-row>
+    </b-jumbotron>
+    
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: String
+  }
+}
+</script>
--- domhall/src/views/Home.vue
+++ domhall/src/views/Home.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="home">
-    <b-card>
-        click about
-    </b-card>
+    <b-jumbotron bg-variant="info" text-variant="dark">
+        <template v-slot:header>My Projects</template>
+    </b-jumbotron>
+    <Project name="LAPI"/>
   </div>
 </template>
 
+
+<script>
+import Project from '@/components/Project.vue'
+
+export default {
+  name: 'about',
+  components: {
+      Project
+  }
+}
+</script>
